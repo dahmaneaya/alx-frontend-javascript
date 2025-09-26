fb792c07ec14f2c76a7ae1564134f63d2ef33dde
Dahmaneaya <dahmanea374@gmail.com>
2025-09-26 18:37:18 +0100

    Add Director and Teacher interfaces, classes, and createEmployee function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -56,6 +56,58 @@ class StudentClass implements StudentClassInterface {
   }
 }
 
+// Define DirectorInterface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// Define TeacherInterface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Implement Director class
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+// Implement Teacher class
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+// Create the createEmployee function
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
+
 // Create teacher instances
 const teacher3: Teacher = {
   firstName: 'John',
@@ -91,6 +143,19 @@ console.log(student1.workOnHomework()); // Should output: Currently working
 const student2: StudentClassInterface = new StudentClass("Bob", "Smith");
 console.log(student2.displayName()); // Should output: Bob
 console.log(student2.workOnHomework()); // Should output: Currently working
+
+// Test the createEmployee function
+console.log(createEmployee(200));   // Should output: Teacher
+console.log(createEmployee(1000));  // Should output: Director  
+console.log(createEmployee('$500')); // Should output: Director
+
+// Test the methods of created employees
+const employee1 = createEmployee(200);
+console.log(employee1.workFromHome()); // Should output: Cannot work from home
+
+const employee2 = createEmployee(1000);
+console.log(employee2.workFromHome()); // Should output: Working from home
+console.log(employee2.getCoffeeBreak()); // Should output: Getting a coffee break
 
 // Additional examples
 const teacher1: Teacher = {
